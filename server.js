45d5e6cbd4f191c6c719effc9ebaefa925da6477
AJ <ascott1083@yahoo.com>
2020-01-10 15:39:19 -0600

    update

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-const express = require("express");
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 
-const PORT = process.env.PORT || 8000;
-const app = express();
+var app = express();
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-var exphbs = require("express-handlebars");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controllers.js");
-
+var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 app.listen(PORT, function() {
-  console.log("Listening on port:%s", PORT);
+  console.log("Server listening on: http://localhost:" + PORT);
 });
