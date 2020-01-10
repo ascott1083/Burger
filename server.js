73d930de18de339ea934e26f952df7ebcefec048
AJ <ascott1083@yahoo.com>
2020-01-09 20:28:10 -0600

    added routes to server

--- server.js
+++ server.js
@@ -2,3 +2,21 @@ const express = require("express");
 
 const PORT = process.env.PORT || 8000;
 const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Listening on port:%s", PORT);
+});
