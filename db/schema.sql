cd1ba7b0da0631bfd98d5f887b31d2c2e75ed8cc
AJ <ascott1083@yahoo.com>
2020-01-12 14:02:40 -0600

    delted files

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS burgers;
 
 CREATE TABLE burgers (
     id int NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN DEFAULT false,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOL DEFAULT false,
     PRIMARY KEY (id)
 );
